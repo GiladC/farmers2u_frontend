b4dd565e9263de04a3a869904900cf0080a42257
GiladC <giladcochavi@gmail.com>
2023-08-29 13:15:22 +0300

    f

--- src/Pages/Forms/FormLogin.js
+++ src/Pages/Forms/FormLogin.js
@@ -18,6 +18,8 @@ const themeForButton = createTheme({
 
 const FormLogin = (props) => {
   const [user, setUser] = useState({});
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState('');
   const [buttonText, setButtonText] = useState('התחבר עם Google');
   const [showPopup, setShowPopup] = useState(false);
